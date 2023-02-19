c354ab1da420f127a7a432c8e6130a8d1e50eab8
Antony Gordon <antgordon18@outlook.com>
2023-02-18 20:40:37 -0500

    Add home page

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,34 +1,22 @@
-﻿namespace MauiApp1;
+﻿using System.Windows.Input;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	public ICommand NavigationCommand { get; private set; }
 
-	public MainPage()
+    public ICommand TestCommand { get; private set; }
+
+    public MainPage()
 	{
-		InitializeComponent();
-		//this.BackgroundColor = Color.FromRgb(0.0, 0.0, 0.0);
+        NavigationCommand = new Command<string>(async (string route) =>
+        {
+            await Shell.Current.GoToAsync(route);
+        });
+
+        InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-    private async void OnNavigateClicked(object sender, EventArgs e)
-    {
-        var navigationParameter = new Dictionary<string, object>
-		{
-			{ "QueryText", "From main page" }
-		};
-        await Shell.Current.GoToAsync($"//AboutBinding", navigationParameter);
-    }
 }
 
