e22996c740e6e5f5464a2ffe5d8f658d1e6352ac
Antony Gordon <antgordon18@outlook.com>
2023-02-25 19:15:13 -0500

    Fix compatibility with number fields that have text

---
+++ MauiApp1/Utility/ExtendedDoubleJsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MauiApp1.Utility
+{
+    public class ExtendedDoubleJsonConverter : JsonConverter<double?>
+    {
+        public override double? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (double.TryParse(reader.GetString(), out double num)) 
+            {
+                return num;
+            } 
+            else 
+            {
+                return null;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, double? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteNumberValue(value.Value);
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+        }
+    }
+}
